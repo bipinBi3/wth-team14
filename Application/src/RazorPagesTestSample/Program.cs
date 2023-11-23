4746c8ef5f4bab591197abd36ba129acb4ce852d
MissyPunk <68071413+MissyPunk@users.noreply.github.com>
2023-11-23 13:40:46 +0000

    Update Program.cs

--- Application/src/RazorPagesTestSample/Program.cs
+++ Application/src/RazorPagesTestSample/Program.cs
@@ -46,4 +46,4 @@ namespace RazorPagesTestSample
                 });
     }
 }
-// basdasdasdasdasdasdaaaaaaaaaaaaaaaaaaaaaaaaaaaaslashasd
+// basdasdasdasdasdasdaaaaaaaaaaaaaaaaaaaaaaaaaaaaslashasdfinal
