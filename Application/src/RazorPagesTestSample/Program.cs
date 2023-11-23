427235c6cc216f7f76d6ef57b92683df89fb30ac
MissyPunk <68071413+MissyPunk@users.noreply.github.com>
2023-11-23 14:31:25 +0000

    Update Program.cs

--- Application/src/RazorPagesTestSample/Program.cs
+++ Application/src/RazorPagesTestSample/Program.cs
@@ -46,4 +46,4 @@ namespace RazorPagesTestSample
                 });
     }
 }
-// people  watching me type makes me self conscuousasddk
+// people  watching me type makes me self conscuousasddk GOD DAMN IT
